305985325d0c984c2923d8d1c04595e64fef5b6f
jibreel <jibreel.powell@gmail.com>
2017-03-23 21:55:39 -0400

    initial commit

---
+++ app/src/main/java/jibreelpowell/com/redbluegreen/MainActivity.java
@@ -0,0 +1,16 @@
+package jibreelpowell.com.redbluegreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
